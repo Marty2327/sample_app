fbc73bc67bd416ef8682783b35af16966f4f42b7
Matt Martindale <marty2327@gmail.com>
2012-09-26 20:35:33 -0500

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,12 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -26,7 +31,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "#{base_title} | Help")
+      page.should have_selector('title', :text => "#{base_title}")
     end
   end
 
@@ -39,7 +44,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "#{base_title} | About Us")
+      page.should have_selector('title', :text => "#{base_title}")
     end
   end
 
@@ -52,7 +57,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text => "#{base_title} | Contact")
+      page.should have_selector('title', :text => "#{base_title}")
     end
   end
 end
