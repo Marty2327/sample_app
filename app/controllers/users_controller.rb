2ef249299796efee8237bf65099c043534cca150
Matt Martindale <marty2327@gmail.com>
2012-10-17 19:52:30 -0500

    Finished user signup w ssl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new  #this adds a init user so the form can modify and save a user
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
